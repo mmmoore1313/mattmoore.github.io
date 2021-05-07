32addf132b87a7faa70bc681ef08d563038fff03
Matt Moore <Matt.M.Moore13@outlook.com>
2021-05-07 13:40:35 -0400

    Removed the unused Button and Nav variables

--- src/components/routes/Home/Home.js
+++ src/components/routes/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 import Layout from '../../shared/layout'
-import { Card, Button, Nav } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
 const Home = () => (
   <Layout>
