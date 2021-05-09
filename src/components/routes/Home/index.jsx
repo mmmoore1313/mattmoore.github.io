6af2af18cad3e92ddc402a5021b77cd5707a9b57
Matt Moore <Matt.M.Moore13@outlook.com>
2021-05-09 04:16:07 -0400

    Added a title with my name, message, and profession; added a picture

--- src/components/routes/Home/index.jsx
+++ src/components/routes/Home/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Layout from '../../shared/layout'
+import { Button } from 'react-bootstrap'
+import '../../style.scss'
 
 const Home = () => {
   return (
-    <React.Fragment>
-      Home component
-    </React.Fragment>
-  )
-}
+  <Layout>
+    <div className='home'>
+      <div className='title'>
+        <h1>
+          <p>Hi!</p>
+          <p>My name is Matt</p>
+          <p>Software Engineer</p>
+        </h1>
+        <Link to='about'>
+          <Button>More Info</Button>
+        </Link>
+      </div>
+      <div className='person'>
+        <img
+          src={`${process.env.PUBLIC_URL}/donkey.jpg`}
+        />
+      </div>
+    </div>
+  </Layout>
+)}
 
 export default Home
