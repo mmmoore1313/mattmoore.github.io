a7f6afa886c484640e8d59df849109685f81a885
Matt Moore <Matt.M.Moore13@outlook.com>
2021-05-09 04:49:42 -0400

    Added an About/ to reflect the new structure

---
+++ src/components/About/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../shared/layout'
+import { Card } from 'react-bootstrap'
+
+const About = () => (
+  <Layout>
+    <Card.Title>Who Is Matt Moore?</Card.Title>
+    <Card.Text>This Is the About Me Card</Card.Text>
+  </Layout>
+)
+
+export default About
