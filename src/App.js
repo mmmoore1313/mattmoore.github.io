633d5f822148851bb95d2a99f8452ace31fef7b9
Matt Moore <Matt.M.Moore13@outlook.com>
2021-05-09 04:44:21 -0400

    Changed the home and about routes to reflect the new structure

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 import "bootstrap/scss/bootstrap.scss";
 
 // routes
-import Home from './components/routes/Home/Home'
-import About from './components/routes/About'
+import Home from './components/Home/'
+import About from './components/About/'
 
 function App(props) {
   return (
