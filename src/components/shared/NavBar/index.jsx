ef8b4c690a1945ca06c8f903f8bf2664c08df241
Matt Moore <Matt.M.Moore13@outlook.com>
2021-05-10 03:42:37 -0400

    Cleared the original Header data and followed the tutorial with Dwinatech. Added buttons and styling that is responsive to the resizing of the screen; added menu options

--- src/components/shared/NavBar/index.jsx
+++ src/components/shared/NavBar/index.jsx
@@ -1,29 +1,91 @@
-import React, { Fragment } from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './style.scss'
 
-const alwaysOptions = (
-  <Fragment>
-    <Nav.Link href="/">Home</Nav.Link>
-    <Nav.Link href="About">About</Nav.Link>
-  </Fragment>
-)
-
-const Header = () => {
+const NavBar = () => {
+  const [open, setOpen] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(0)
+  
+  const trackScreenWidth = () => {
+    const width = window.innerWidth
+    setScreenWidth(width)
+    if (width > 800) {
+      setOpen(true)
+    }
+  }
+  
+  useEffect(() => {
+    trackScreenWidth()
+    window.addEventListener("resize", trackScreenWidth)
+    return () => window.removeEventListener('resize', trackScreenWidth)
+  }, [])
+  
   return (
-    <Navbar bg="primary" variant="dark" expand="md">
-      <Navbar.Brand href="#">
-        Matt Moore
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          {/* user && <span className="navbar-text mr-2">Welcome, {user.email}</span>*/}
-          { alwaysOptions }
-          {/* user ? authenticatedOptions : unauthenticatedOptions */}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <nav className='navbar'>
+      <div className='nav-wrapper'>
+        <div className='logo'>
+          <Link to='/'>
+            Home
+          </Link>
+        </div>
+        <div className='list-wrapper'>
+          <img 
+            src='https://github.com/DwinaTech/public-images/blob/main/menu-bars.png?raw=true'
+            alt='close'
+            style={{ opacity: !open ? 1 : 0 }}
+            onClick={() => {
+              setOpen(!open)
+            }}
+          />
+          <img 
+            src='https://github.com/DwinaTech/public-images/blob/main/cross-menu-icon.png?raw=true'
+            alt='menu'
+            style={{ opacity: open ? 1 : 0 }}
+            onClick={() => {
+              setOpen(!open)
+            }}
+          />
+          <ul style={{ left: open ? "0" : "-100vw" }}>
+            <li>
+              <Link
+                to='/'
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/about'
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/projects'
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/tutorials'
+              >
+                Tutorials
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/blog'
+              >
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
-export default Header
+export default NavBar
