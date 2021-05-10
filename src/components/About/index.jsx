527d4f2b0c1fbb6df069915bea0c30ac4d8d0176
Matt Moore <Matt.M.Moore13@outlook.com>
2021-05-10 04:43:38 -0400

    I didn't think that Skills needed to be it's own entity; added that section to the About page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Layout from '../shared/layout'
 import { Card } from 'react-bootstrap'
+import Skills from '../Skills/'
 
 const About = () => {
   return (
     <Layout className='about'>
       <Card.Title>Who Is Matt Moore?</Card.Title>
       <Card.Text>This Is the About Me Card</Card.Text>
+      <Skills />
     </Layout>
   )
 }
